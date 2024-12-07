3a4d4fbbcda4658913ca4d41a4287a4b2cab9ef2
isikepalaku <isikepalakreatif@gmail.com>
2024-12-07 17:17:50 +0800

    Menambahkan fitur baru

--- backend/open_webui/apps/openai/main.py
+++ backend/open_webui/apps/openai/main.py
@@ -148,8 +148,8 @@ async def speech(request: Request, user=Depends(get_verified_user)):
         headers["Authorization"] = f"Bearer {app.state.config.OPENAI_API_KEYS[idx]}"
         headers["Content-Type"] = "application/json"
         if "openrouter.ai" in app.state.config.OPENAI_API_BASE_URLS[idx]:
-            headers["HTTP-Referer"] = "https://openwebui.com/"
-            headers["X-Title"] = "Open WebUI"
+            headers["HTTP-Referer"] = "https://reserse.id/"
+            headers["X-Title"] = "Reserse AI"
         if ENABLE_FORWARD_USER_INFO_HEADERS:
             headers["X-OpenWebUI-User-Name"] = user.name
             headers["X-OpenWebUI-User-Id"] = user.id
