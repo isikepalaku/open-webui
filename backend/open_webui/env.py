ac3238a2b5e4a18ac7db4f43d44679ec5bed41ab
isikepalaku <isikepalakreatif@gmail.com>
2024-12-27 22:31:49 +0800

    new

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -101,9 +101,9 @@ log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
 if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+    WEBUI_NAME += " - Penyidik Ai"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://chat.reserse.id/favicon/favicon.ico"
 
 
 ####################################
